4b3ad893301495cab970bafb31aa8164e6563255
chandu1987 <ircsekhar@gmail.com>
2025-07-23 22:58:31 -0400

    Added Double to String Conversion

---
+++ Sources/RCSUI/Extensions/Double+Extensions.swift
@@ -0,0 +1,7 @@
+//
+//  Double+Extensions.swift
+//  RCSUI
+//
+//  Created by Chandra Sekhar Ravi on 2025-07-23.
+//
+
---
+++ Sources/RCSUI/Extensions/Int+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extention+Int.swift
+//  RCSUI
+//
+//  Created by Chandra Sekhar Ravi on 2025-07-22.
+//
+import Foundation
+
+public extension Int {
+    func toString() -> String {
+        return String(self)
+    }
+}
